a2c07f04861f37f76d4306697f1f1538ef098b3f
Cy Brown <cy.brown59@gmail.com>
2016-08-07 23:03:49 +0200

    Montrer l'aide si la commande est incorrect

--- webcli.js
+++ webcli.js
@@ -1,6 +1,15 @@
 var packageInfo = require('./package.json');
 var commander = require('commander');
 
+var commandCalled = false;
+
+function commandWrapper(command) {
+    return function () {
+        commandCalled = true;
+        return command.apply(this, arguments);
+    };
+}
+
 commander
     .version(packageInfo.version);
 
@@ -10,14 +19,14 @@ commander
     .description('Run development server')
     .option('-p, --port <port>', 'Port for development server')
     .option('-e --env <environment>', 'Specify environment, dev or prod', /^(dev|prod)$/i, 'dev')
-    .action(require('./lib/commands/server'));
+    .action(commandWrapper(require('./lib/commands/server')));
 
 commander
     .command('build')
     .alias('b')
     .description('Create production files')
     .option('-e --env <environment>', 'Specify environment, dev or prod', /^(dev|prod)$/i, 'prod')
-    .action(require('./lib/commands/build'));
+    .action(commandWrapper(require('./lib/commands/build')));
 
 commander
     .command('package')
@@ -25,21 +34,26 @@ commander
     .description('Create production package')
     .option('-t --type [type]', 'Create an archive (zip or tgz), defaults to zip', /^(zip|tgz)$/i, 'zip')
     .option('-e --env <environment>', 'Specify environment, dev or prod', /^(dev|prod)$/i, 'prod')
-    .action(require('./lib/commands/package'));
+    .action(commandWrapper(require('./lib/commands/package')));
 
 commander
     .command('test')
+    .alias('t')
     .description('Run unit tests')
     .option('-e --env <environment>', 'Specify environment, dev or prod', /^(dev|prod)$/i, 'dev')
-    .action(require('./lib/commands/test'));
+    .action(commandWrapper(require('./lib/commands/test')));
 
 commander
     .command('clean')
     .description('Remove build files')
-    .action(require('./lib/commands/clean'));
+    .action(commandWrapper(require('./lib/commands/clean')));
 
 commander.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
+    commander.outputHelp();
+}
+
+if (!commandCalled) {
     commander.outputHelp();
 }
