6a4896d0d4f0dea8d1e86560723fea681010c08c
Cy Brown <cy.brown59@gmail.com>
2016-08-08 20:30:10 +0200

    Ne pas afficher les informations de webpack pendant les tests

--- config/karma.test.conf.js
+++ config/karma.test.conf.js
@@ -75,6 +75,8 @@ module.exports = function (config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    webpackServer: { noInfo: true }
   })
 }
