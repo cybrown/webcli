3d6043d91fe9c4d819c3c170b2ac006337daee6e
Cy Brown <cy.brown59@gmail.com>
2016-08-10 13:56:17 +0200

    Ajout d'un mode verbose et de logs

--- webcli.js
+++ webcli.js
@@ -1,17 +1,36 @@
 var packageInfo = require('./package.json');
 var commander = require('commander');
+var winston = require('winston');
 
 var commandCalled = false;
 
-function commandWrapper(command) {
-    return function () {
+var logger = new (winston.Logger)({
+    transports: [
+      new winston.transports.Console()
+    ]
+});
+
+logger.cli();
+
+function commandWrapper(commandName) {
+    return function (params) {
+        var command = require('./lib/commands/' + commandName);
         commandCalled = true;
-        return command.apply(this, arguments);
+        if (commander.verbose) {
+            logger.transports.console.level = 'verbose';
+        }
+        logger.verbose('Verbose mode enabled');
+        if (params.env) {
+            logger.verbose('Environment:', params.env);
+        }
+        logger.verbose('Running command:', commandName);
+        return command.apply(this, [params].concat(logger));
     };
 }
 
 commander
-    .version(packageInfo.version);
+    .version(packageInfo.version)
+    .option('-v, --verbose', 'Enable verbose mode');
 
 commander
     .command('server')
@@ -19,14 +38,14 @@ commander
     .description('Run development server')
     .option('-p, --port <port>', 'Port for development server')
     .option('-e --env <environment>', 'Specify environment, dev or prod', /^(dev|prod)$/i, 'dev')
-    .action(commandWrapper(require('./lib/commands/server')));
+    .action(commandWrapper('server'));
 
 commander
     .command('build')
     .alias('b')
     .description('Create production files')
     .option('-e --env <environment>', 'Specify environment, dev or prod', /^(dev|prod)$/i, 'prod')
-    .action(commandWrapper(require('./lib/commands/build')));
+    .action(commandWrapper('build'));
 
 commander
     .command('package')
@@ -34,7 +53,7 @@ commander
     .description('Create production package')
     .option('-t --type [type]', 'Create an archive (zip or tgz), defaults to zip', /^(zip|tgz)$/i, 'zip')
     .option('-e --env <environment>', 'Specify environment, dev or prod', /^(dev|prod)$/i, 'prod')
-    .action(commandWrapper(require('./lib/commands/package')));
+    .action(commandWrapper('package'));
 
 commander
     .command('test')
@@ -42,17 +61,15 @@ commander
     .description('Run unit tests')
     .option('-c --coverage', 'Enable coverage')
     .option('-r --test-report', 'Output a test result report')
-    .action(commandWrapper(require('./lib/commands/test')));
+    .action(commandWrapper('test'));
 
 commander
     .command('clean')
     .description('Remove build files')
-    .action(commandWrapper(require('./lib/commands/clean')));
+    .action(commandWrapper('clean'));
 
 commander.parse(process.argv);
 
-if (!process.argv.slice(2).length) {
-    commander.outputHelp();
-} else if (!commandCalled) {
+if (!process.argv.slice(2).length || !commandCalled) {
     commander.outputHelp();
 }
